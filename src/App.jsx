309c6bcf5480b00650e859e6007e7341657bd8b7
Luli <lucilapastore@gmail.com>
2024-05-13 21:54:45 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { ProjectsSideBar } from "./components/ProjectsSideBar.jsx";
+
+function App() {
+  return (
+    <main className="h-screen my-8">
+      <ProjectsSideBar />
+    </main>
+  );
+}
+
+export default App;
