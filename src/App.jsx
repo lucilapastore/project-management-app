3b3f1f56762d25c69a655c736f50d0f54bb873de
Luli <lucilapastore@gmail.com>
2024-05-23 19:23:40 -0300

    refactor: split components to split JSX and tailwind styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import NewProject from "./components/NewProject.jsx";
+import NoProjectSelected from "./components/NoProjectSelected.jsx";
 import { ProjectsSideBar } from "./components/ProjectsSideBar.jsx";
 
 function App() {
@@ -6,7 +6,7 @@ function App() {
     <main className="h-screen my-8 flex gap-8">
       {" "}
       <ProjectsSideBar />
-      <NewProject />
+      <NoProjectSelected />
     </main>
   );
 }
