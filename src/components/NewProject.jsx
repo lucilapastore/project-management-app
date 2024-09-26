3794436dc418a887cb33801408711730872f7f6a
lucilapastore <lucilapastore@gmail.com>
2024-09-26 13:22:48 -0300

    feat: add cancel button

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -4,7 +4,7 @@ import Modal from "./Modal.jsx";
 
 //validation...
 
-export default function NewProject({ onAdd }) {
+export default function NewProject({ onAdd, onCancel }) {
   const modal = useRef();
   const title = useRef();
   const description = useRef();
@@ -48,7 +48,10 @@ export default function NewProject({ onAdd }) {
       <div className="w-[35rem] mt-16">
         <menu className="flex items-center justify-end gap-4 my-4">
           <li>
-            <button className="text-stone-800 hover:text-stone-950">
+            <button
+              className="text-stone-800 hover:text-stone-950"
+              onClick={onCancel}
+            >
               Cancel
             </button>
           </li>
